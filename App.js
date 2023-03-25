4e3cd1530ad439a9dce286c4db27d250bef3c545
Natalia Shuvaeva <glukomani@gmail.com>
2023-03-26 00:11:35 +0100

    Add screen UI

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Ionicons from 'react-native-vector-icons/Ionicons'
@@ -6,9 +7,97 @@ import Analysis from './src/screens/Analysis'
 import Statistics from './src/screens/Statistics'
 import Settings from './src/screens/Settings'
 
+import * as SQLite from 'expo-sqlite'
+
+const db = SQLite.openDatabase('db.foodDb') // returns Database object
+
 const Tab = createBottomTabNavigator()
 
 export default function App() {
+  const [reactionData, setReactionData] = useState(null)
+  const [foodData, setFoodData] = useState(null)
+
+  // db.transaction((tx) => {
+  //   tx.executeSql('DROP TABLE recordlist')
+  // })
+
+  // Check if the items table exists if not create it
+  db.transaction((tx) => {
+    tx.executeSql(
+      'CREATE TABLE IF NOT EXISTS foodlist (id INTEGER PRIMARY KEY AUTOINCREMENT, value TEXT, timestamp TEXT)',
+    )
+  })
+
+  db.transaction((tx) => {
+    tx.executeSql(
+      'CREATE TABLE IF NOT EXISTS reaction (id INTEGER PRIMARY KEY AUTOINCREMENT, value TEXT, timestamp TEXT, notes TEXT)',
+    )
+  })
+
+  // event handler for new item creation
+  const newFoodRecord = (name, timestamp) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        'INSERT INTO foodlist (value, timestamp) values (?, ?)',
+        [name, timestamp],
+        (txObj, resultSet) =>
+          setData(
+            data.concat({
+              id: resultSet.insertId,
+              value: name,
+              timestamp: timestamp,
+            }),
+          ),
+        (txObj, error) => console.log('Error', error),
+      )
+    })
+  }
+
+  // event handler for new item creation
+  const newReactionRecord = (value, timestamp) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        'INSERT INTO reaction (value, timestamp) values (?, ?)',
+        [value, timestamp],
+        (txObj, resultSet) =>
+          setData(
+            data.concat({
+              id: resultSet.insertId,
+              value: value,
+              timestamp: timestamp,
+            }),
+          ),
+        (txObj, error) => console.log('Error', error),
+      )
+    })
+  }
+
+  const fetchData = () => {
+    db.transaction((tx) => {
+      // sending 4 arguments in executeSql
+      tx.executeSql(
+        'SELECT * FROM foodlist',
+        null, // passing sql query and parameters:null
+        // success callback which sends two things Transaction object and ResultSet Object
+        (txObj, { rows: { _array } }) => setFoodData(_array),
+        // failure callback which sends two things Transaction object and Error
+        (txObj, error) => console.log('Error ', error),
+      )
+    })
+
+    db.transaction((tx) => {
+      // sending 4 arguments in executeSql
+      tx.executeSql(
+        'SELECT * FROM reaction',
+        null, // passing sql query and parameters:null
+        // success callback which sends two things Transaction object and ResultSet Object
+        (txObj, { rows: { _array } }) => setReactionData(_array),
+        // failure callback which sends two things Transaction object and Error
+        (txObj, error) => console.log('Error ', error),
+      )
+    })
+  }
+
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -27,15 +116,21 @@ export default function App() {
             }
             return <Ionicons name={iconName} size={size} color={color} />
           },
-          tabBarActiveTintColor: '#ec9706',
-          tabBarInactiveTintColor: 'gray',
+          tabBarActiveTintColor: '#808000',
+          tabBarInactiveTintColor: '#cccc99',
         })}
       >
-        <Tab.Screen options={{ headerShown: false }} name="Add" children={() => <Add />} />
+        <Tab.Screen
+          options={{ headerShown: false }}
+          name="Add"
+          children={() => (
+            <Add newReactionRecord={newReactionRecord} newFoodRecord={newFoodRecord} />
+          )}
+        />
         <Tab.Screen
           options={{ headerShown: false }}
           name="Statistics"
-          children={() => <Statistics />}
+          children={() => <Statistics reactionData={reactionData} foodData={foodData} />}
         />
         <Tab.Screen
           options={{ headerShown: false }}
@@ -45,7 +140,7 @@ export default function App() {
         <Tab.Screen
           options={{ headerShown: false }}
           name="Settings"
-          children={() => <Settings />}
+          children={() => <Settings fetchData={fetchData} />}
         />
       </Tab.Navigator>
     </NavigationContainer>
--- src/screens/Add.js
+++ src/screens/Add.js
@@ -1,10 +1,181 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useCallback, useState } from 'react'
+import { View, Text, ScrollView, Pressable } from 'react-native'
+import moment from 'moment'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import MultiSlider from '@ptomasroos/react-native-multi-slider'
+import Autocomplete from 'react-native-autocomplete-input'
 
 export const Add = (props) => {
+  const [date, setDate] = useState(moment().format('DD.MM.YYYY'))
+  const [reaction, setReaction] = useState(0)
+  const [text, setText] = useState('')
+  const [foods, setFoods] = useState(['кофе', 'лук'])
+
+  const displayDate = useCallback(() => {
+    console.log('date', date)
+    const today = moment()
+    console.log('today: ', today)
+    const isToday = moment(date, 'DD.MM.YYYY').isSame(today, 'day')
+    console.log('isToday: ', isToday)
+
+    return '  ' + (isToday ? 'Today' : date) + '  '
+  }, [date])
+
+  const handleSending = useCallback(() => {
+    const res = {
+      date,
+    }
+
+    // sending
+    console.log('res', res)
+  }, [date])
+
+  const renderDayPicker = () => {
+    return (
+      <View
+        style={{
+          paddingTop: '7%',
+          paddingBottom: '7%',
+          flexDirection: 'row',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Pressable
+          onPress={() => {
+            console.log('press back')
+            onArrowPress('back')
+          }}
+        >
+          <Ionicons name={'caret-back-outline'} size={25} color={'black'} />
+        </Pressable>
+        <Text style={{ fontSize: 20 }}>{displayDate()}</Text>
+        <Pressable
+          onPress={() => {
+            console.log('press forward')
+            onArrowPress('forward')
+          }}
+          disabled={moment(date, 'DD.MM.YYYY').isSame(moment(), 'day')}
+        >
+          <Ionicons name={'caret-forward-outline'} size={25} color={'black'} />
+        </Pressable>
+      </View>
+    )
+  }
+
+  const filterData = useCallback(
+    (text) => {
+      let data = []
+      foods.forEach((item, index) => {
+        if (text.length > 1 && item.includes(text.toLowerCase())) {
+          const newKey = item + index
+          data.push({
+            id: itemOfData.key,
+            key: newKey,
+            word: item,
+          })
+        }
+      })
+      return data.sort((a, b) => b.color - a.color)
+    },
+    [props.colors],
+  )
+
+  const renderFoodList = (item) => {
+    return <Text>{item}</Text>
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Add</Text>
+    <View
+      style={{
+        backgroundColor: '#f6f6f6',
+        height: '100%',
+        width: '100%',
+      }}
+    >
+      <View
+        style={{
+          height: '5%',
+          flexDirection: 'row',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      />
+      <View
+        style={{
+          height: '78%',
+          flex: 1,
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          paddingLeft: '10%',
+          paddingRight: '10%',
+        }}
+      >
+        {renderDayPicker()}
+
+        <MultiSlider onValuesChange={setReaction} sliderLength={300} min={0} max={11} step={1} />
+        <Text>{reaction}</Text>
+
+        <View style={{ flexDirection: 'row', marginTop: '10%' }}>
+          <View style={{ width: '80%' }}>
+            <Autocomplete
+              style={{ paddingLeft: '5%' }}
+              // inputContainerStyle={styles.input}
+              // containerStyle={styles.containerStyle}
+              placeholder={'food name'}
+              defaultValue={text}
+              data={filterData(text)}
+              onChangeText={setText}
+              autoCorrect={false}
+              // listStyle={styles.results}
+              renderItem={({ item }) => {
+                return (
+                  <TouchableOpacity
+                    onPress={() => {
+                      setText(``)
+                    }}
+                  >
+                    <Text key={item.word}>{item.word}</Text>
+                  </TouchableOpacity>
+                )
+              }}
+            />
+          </View>
+          <Pressable
+            style={{
+              marginLeft: '5%',
+              backgroundColor: '#808000',
+              borderRadius: '5',
+              padding: '4%',
+            }}
+            onPress={() => {
+              setFoods([...foods, text])
+              setText('')
+            }}
+          >
+            <Text>Add</Text>
+          </Pressable>
+        </View>
+
+        <ScrollView style={{ height: '30%', marginTop: '5%', marginBottom: '5%' }}>
+          {foods.map(renderFoodList)}
+        </ScrollView>
+
+        <Pressable onPress={handleSending}>
+          <Text
+            style={{
+              fontSize: 20,
+              marginBottom: '5%',
+              fontWeight: 700,
+              color: '#ec9706',
+            }}
+          >
+            Send
+          </Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
